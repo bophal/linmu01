a95808698b3ed44bf774de6e0ca4a37705d762d5
bophal <123788548+bophal@users.noreply.github.com>
2026-06-05 16:15:06 +0700

    Add files via upload

---
+++ LiNMU/src/main/java/com/lib/linmu/LiNmuApplication.java
@@ -0,0 +1,13 @@
+package com.lib.linmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiNmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiNmuApplication.class, args);
+	}
+
+}
---
+++ LiNMU/src/test/java/com/lib/linmu/LiNmuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.linmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiNmuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
